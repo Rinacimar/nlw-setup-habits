60d183aa6155a3b676903166a7146f7db47d2854
Rinacimar <rinacimareller@gmail.com>
2023-01-21 14:40:52 -0300

    update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ form.addEventListener("change", save)
 
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
-  // const today = "16/01"
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
@@ -20,20 +19,9 @@ function add() {
 }
 
 function save() {
-  localStorage.setItem('Setup@habits', JSON.stringify(nlwSetup.data))
+  localStorage.setItem("Setup@habits", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem('Setup@habits'))
+const data = JSON.parse(localStorage.getItem("Setup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
-
-/*
-const data = {
-  water: ["01-02", "01-06"],
-  gym: ["01-02", "01-03", "01-04", "01-05", "01-06", "01-09", "01-10", "01-11"],
-  sleep: ["01-01", "01-06"],
-  food: ["01-06"],
-}
-
-
-*/
